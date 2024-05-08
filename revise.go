ef099626c4006fa04a1c8dcc3e9dcfc85fb4fdae
LufDevOps <devops.hoanghagiang@gmail.com>
2024-05-08 16:12:27 +0700

    practice thu tu

---
+++ revise.go
@@ -0,0 +1,31 @@
+package main
+import "fmt"
+
+func main(){
+	var a = "inital"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b,c)
+
+	var d,e int = 3,4
+	fmt.Println(e, d)
+
+	var g,h int = 9,10
+	fmt.Println(g,h)
+
+	var d1 = true;
+	fmt.Println(d1)
+	var d2 = false;
+	fmt.Println(d2)
+
+	var inital int
+	fmt.Println(inital)
+
+	var o int
+	fmt.Println(o)
+
+	f := "water melon"
+	fmt.Println(f)
+	
+}
